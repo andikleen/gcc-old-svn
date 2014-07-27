83e4e1ac013ccbddc7d299eb2a2fd664bc63c414
Andi Kleen <ak@linux.intel.com>
2014-07-27 08:45:50 -0700

    Add a spooky implementation for inchash for 64bit hosts
    Reimplement the inchash interface with the spooky hash for 64bit
    hosts.   Spooky is optimized for long hashes and avoids redoing a
    full hash computation for every input value. It's also optimized for 64bit.
    This allows us to exploit the full 64bit computing capability.
    For 32bit we stay with the old jhash.
    
    XXX numbers
    
    gcc/:
    
    2014-07-30  Andi Kleen  <ak@linux.intel.com>
    
    	* inchash.h (hash): Rename to inchash_jhash.
    	(hash_spoody): Add a new spooky hash based implementation.
    	(hash): Add new child class with common functions.

--- gcc/inchash.h
+++ gcc/inchash.h
@@ -28,9 +28,7 @@ along with GCC; see the file COPYING3.  If not see
 /* This file implements an incremential hash function ADT, to be used
    by code that incrementially hashes a lot of unrelated data
    (not in a single memory block) into a single value. The goal
-   is to make it easy to plug in efficient hash algorithms.
-   Currently it just implements the plain old jhash based
-   incremental hash from gcc's tree.c.  */
+   is to make it easy to plug in efficient hash algorithms. */
 
 extern hashval_t iterative_hash_host_wide_int (HOST_WIDE_INT, hashval_t);
 extern hashval_t iterative_hash_hashval_t (hashval_t, hashval_t);
@@ -38,15 +36,16 @@ extern hashval_t iterative_hash_hashval_t (hashval_t, hashval_t);
 namespace inchash
 {
 
-class hash
+/* inchash using plain old jhash from gcc's tree.c.  */
+
+class hash_jhash
 {
  public:
 
   /* Start incremential hashing, optionally with SEED.  */
-  hash (hashval_t seed = 0)
+  hash_jhash (hashval_t seed = 0)
   {
     val = seed;
-    bits = 0;
   }
 
   /* End incremential hashing and provide the final value.  */
@@ -67,13 +66,7 @@ class hash
     val = iterative_hash_host_wide_int (v, val);
   }
 
-  /* Hash in pointer PTR.  */
-  void add_ptr (void *ptr)
-  {
-    add (&ptr, sizeof (ptr));
-  }
-
-  /* Add a memory block DATA with size LEN.  */
+   /* Add a memory block DATA with size LEN.  */
   void add (const void *data, size_t len)
   {
     val = iterative_hash (data, len, val);
@@ -86,7 +79,7 @@ class hash
   }
 
   /* Hash in state from other inchash OTHER.  */
-  void merge (hash &other)
+  void merge (hash_jhash &other)
   {
     merge_hash (other.val);
   }
@@ -96,24 +89,11 @@ class hash
     add (&obj, sizeof(T));
   }
 
-  /* Support for accumulating boolean flags */
-
-  void add_flag (bool flag)
-  {
-    bits = (bits << 1) | flag;
-  }
-
-  void commit_flag ()
-  {
-    add_int (bits);
-    bits = 0;
-  }
-
   /* Support for commutative hashing. Add A and B in a defined order
      based on their value. This is useful for hashing commutative
      expressions, so that A+B and B+A get the same hash.  */
 
-  void add_commutative (hash &a, hash &b)
+  void add_commutative (hash_jhash &a, hash_jhash &b)
   {
     if (a.end() > b.end())
       {
@@ -129,6 +109,134 @@ class hash
 
  private:
   hashval_t val;
+};
+
+/* Use spooky hash on a 64bit host. This avoids doing all work on
+   every update.  */
+
+#include "spooky.h"
+
+class hash_spooky
+{
+ public:
+  /* Start incremential hashing, optionally with SEED.  */
+  hash_spooky  (hashval_t seed = 0)
+  {
+   sp.Init (seed, seed);
+  }
+
+  /* End incremential hashing and provide the final value.  */
+  hashval_t end ()
+  {
+    uint64_t a, b;
+    sp.Final (&a, &b);
+    return (hashval_t) a;
+  }
+
+  /* Add unsigned value V.  */
+  void add_int (unsigned v)
+  {
+    sp.Update (&v, sizeof (v));
+  }
+
+  /* Add HOST_WIDE_INT value V.  */
+  void add_wide_int (HOST_WIDE_INT v)
+  {
+    sp.Update (&v, sizeof (v));
+  }
+
+   /* Add a memory block DATA with size LEN.  */
+  void add (const void *data, size_t len)
+  {
+    sp.Update (data, len);
+  }
+
+  /* Merge hash value OTHER.  */
+  void merge_hash (hashval_t other)
+  {
+    sp.Update (&other, sizeof (hashval_t));
+  }
+
+  /* Hash in state from other inchash OTHER.  */
+  void merge (hash_spooky &other)
+  {
+    uint64_t a, b;
+    other.sp.Final (&a, &b);
+    add_wide_int (a);
+    add_wide_int (b);
+  }
+
+  /* Support for commutative hashing. Add A and B in a defined order
+     based on their value. This is useful for hashing commutative
+     expressions, so that A+B and B+A get the same hash.  */
+
+  void add_commutative (hash_spooky &a, hash_spooky &b)
+  {
+    uint64_t a0, b0, a1, b1;
+
+    a.sp.Final (&a0, &a1);
+    b.sp.Final (&b0, &b1);
+
+    if (a0 > b0 || (a0 == b0 && a1 > b1))
+      {
+	add_wide_int (b0);
+	add_wide_int (b1);
+	add_wide_int (a0);
+	add_wide_int (a1);
+      }
+    else
+      {
+	add_wide_int (a0);
+	add_wide_int (a1);
+	add_wide_int (b0);
+	add_wide_int (b1);
+      }
+  }
+
+ private:
+   Spooky sp;
+};
+
+#if BITS_PER_HOST_WIDE_INT == 64
+#define hash_impl hash_spooky
+#else
+#define hash_impl hash_jhash
+#endif
+
+/* Common utility functions for the inchash classes.  */
+
+class hash : public hash_impl
+{
+ public:
+  hash(hashval_t seed = 0) : hash_impl(seed), bits(0) {}
+
+  /* Hash in pointer PTR.  */
+  void add_ptr (void *ptr)
+  {
+    add (&ptr, sizeof (ptr));
+  }
+
+  template<class T> void add_object(T &obj)
+  {
+    add(&obj, sizeof(T));
+  }
+
+  /* Support for accumulating boolean flags */
+
+  /* Queue flag FLAG to the hash. Must call commit_flag later.  */
+  void add_flag (bool flag)
+  {
+    bits = (bits << 1) | flag;
+  }
+
+  /* Commit multiple flags to the hash.  */
+  void commit_flag ()
+  {
+    add_int (bits);
+    bits = 0;
+  }
+
+ private:
   unsigned bits;
 };
 
